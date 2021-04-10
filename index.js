16546dfdcb319b5757ef3cd7b9e170c6e4190bd2
chris <cseiter@gmail.com>
2021-04-10 14:25:20 -0400

    Book titles fetched, see index.js comments for resolution of errors

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+// return error for CORS resolved by following
+// https://community.airtable.com/t/cors-policy-error-when-trying-to-fetch-api-no-access-control-allow-origin-header/29720
+// and accessing the demo server at
+// https://cors-anywhere.herokuapp.com/corsdemo 
+  return fetch('https://cors-anywhere.herokuapp.com/https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json))
 }
 
 function renderBooks(books) {
